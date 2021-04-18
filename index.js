62babe2336f01621ffd4f85e31220b26fdf4e706
jinook929 <jinook929@gmail.com>
2021-04-17 22:45:38 -0400

    commit tree 0411cf30f69c237ea170daa2a25302a0ba785d26

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(n){
   // Write your code here!
+  if(n <= 400) {
+    return 'This one is on me!'
+  } else if(n > 2500) {
+    return 'No can do.'
+  } else if(n > 2000) {
+    return 'I will gladly take your thirty bucks.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if(tip === "generous") {
+    return 'Thank you so much.'
+  } else if(tip === "not as generous") {
+    return 'Thank you.'
+  } else {
+    return "Bye."
+  }
 }
